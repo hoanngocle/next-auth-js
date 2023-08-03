8d0da52bec36c6a74e12d51f1b6c40ea1a705313
hoanln <hoanln@deha-soft.com>
2023-08-03 10:50:12 +0700

    Add all page auth next

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,16 @@
-import Image from 'next/image';
 import { options } from './api/auth/[...nextauth]/options';
 import { getServerSession } from 'next-auth';
+import UserCard from './components/UserCard';
 
 export default async function Home() {
     const session = await getServerSession(options);
-    return <>{session ? <div>Home</div> : <h1 className='text-5xl'>You Shall Not Pass!</h1>}</>;
+    return (
+        <>
+            {session ? (
+                <UserCard user={session?.user} pagetype='Home' />
+            ) : (
+                <h1 className='text-5xl'>You Shall Not Pass!</h1>
+            )}
+        </>
+    );
 }
